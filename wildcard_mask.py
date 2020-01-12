1aec96bf1329b780f1f1d638fbdfefee83c1c083
dantesmaily <37872451+dantesmaily@users.noreply.github.com>
2020-01-12 16:24:44 -0400

    Upload File

---
+++ wildcard_mask.py
@@ -0,0 +1,11 @@
+def wildcard_mask(netmask):
+    # La mascara digitado deve ser tipo string (no una direccion ipv4)
+	netmask = str(netmask)
+    # Romper la entrada a una lista of octetos como integros
+	netmask_list = map(int, netmask.split("."))
+    #Utilizamos 'XOR' para convertir cada octeto de la mascara
+	wildcard_list = [ 255^octec for octec in netmask_list ]
+	wildcard_mask = ".".join(map(str,wildcard_list))
+	return wildcard_mask
+
+wildcard_mask(str(input("Digite la mascara a convertir ")))
